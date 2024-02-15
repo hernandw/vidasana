63afa0cc5566d47cb16588730bd7fdc66f140240
Williams Hernández <hernandw@gmail.com>
2024-02-15 19:00:34 -0300

    :bug: error en el puerto

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes/routes");
 
-const PORT = procecess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 //Middlewares
